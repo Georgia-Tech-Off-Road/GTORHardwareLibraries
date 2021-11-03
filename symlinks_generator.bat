7f06e30a77483805286e29b97314f4b80244e61e
Ryan Chen <chenrkc030@gmail.com>
2021-11-02 21:17:58 -0400

    Updated symlinks generator batch file

--- symlinks_generator.bat
+++ symlinks_generator.bat
@@ -2,8 +2,18 @@
     echo NOTICE: Make sure to run batch file as administrator    
     set source= %~dp0.
     set /p target= "Enter the path to the Arduino libraries directory: "
-    set exclude=
+    set exclude= 
+
 
     forfiles /P "%source%" /C "cmd /c if @isdir==TRUE ( mklink /d \"%target%\@file\" @path )"
+    forfiles /P "%source%\Sensors" /C "cmd /c if @isdir==TRUE ( mklink /d \"%target%\@file\" @path )"
+    forfiles /P "%source%\ExternalLibraries" /C "cmd /c if @isdir==TRUE ( mklink /d \"%target%\@file\" @path )"
+    forfiles /P "%source%\Utility" /C "cmd /c if @isdir==TRUE ( mklink /d \"%target%\@file\" @path )"
+
+    cmd /c rmdir %target%\Sensors
+    cmd /c rmdir %target%\ExternalLibraries
+    cmd /c rmdir %target%\Utility
+    cmd /c rmdir %target%\.git
+
 
 cmd /k
