5a16773f30ebed2c42e1c207f36f8ee7001146db
Ryan Chen <chenrkc030@gmail.com>
2021-11-05 23:56:28 -0400

    Updated symlinks generator to work with comms overhaul

--- symlinks_generator.bat
+++ symlinks_generator.bat
@@ -5,6 +5,12 @@
     set exclude= 
 
 
+::  these commands try to remove folders along with symlinks. fix it somehow???
+    forfiles /P "%target%" /C "cmd /c if %errorlevel% == 0 echo @fname is a symlink"
+    forfiles /P "%target%" /C "cmd /c if %errorlevel% == 1 echo @fname is a not a symlink"
+    forfiles /P "%target%" /C "cmd /c if %errorlevel% == 0 rmdir @path & echo @fname removed"
+
+
     forfiles /P "%source%" /C "cmd /c if @isdir==TRUE ( mklink /d \"%target%\@file\" @path )"
     forfiles /P "%source%\Sensors" /C "cmd /c if @isdir==TRUE ( mklink /d \"%target%\@file\" @path )"
     forfiles /P "%source%\ExternalLibraries" /C "cmd /c if @isdir==TRUE ( mklink /d \"%target%\@file\" @path )"
@@ -14,6 +20,5 @@
     cmd /c rmdir %target%\ExternalLibraries
     cmd /c rmdir %target%\Utility
     cmd /c rmdir %target%\.git
-
 
 cmd /k
