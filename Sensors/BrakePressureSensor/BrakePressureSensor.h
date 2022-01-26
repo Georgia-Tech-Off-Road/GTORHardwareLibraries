f09e0767b15695820a3768e2af4df30a0837e21f
Andrew Hellrigel <ahellrigel33@gmail.com>
2022-01-25 21:25:07 -0500

    Fixed some documentation and bug in BrakePressureSensor

--- Sensors/BrakePressureSensor/BrakePressureSensor.h
+++ Sensors/BrakePressureSensor/BrakePressureSensor.h
@@ -4,7 +4,7 @@
 
 /**
  * BrakePressureSensor.h
- * A library to work with MLH02KPSB06A sensors and measure the temperature
+ * A library to work with MLH02KPSB06A sensors and measure the brake pressure in PSI
  * 
  * ------ Constructor ------
  * BrakePressureSensor bp;
@@ -14,16 +14,16 @@
  * 
  * ------ Usage ------
  * adc.attach_sensor(bp, port);
- *      - This will attach the LMT87Sensor to an ADC that it can be read from
+ *      - This will attach the BrakePressureSensor to an ADC that it can be read from
  * 
- * int bp_data = bp.get_data();
+ * float bp_data = bp.get_data();
  *      - This will read the data from the ADC that the BrakePressureSensor is attached to and return it
  * 
  * bp.update();
  *      - This will update the sensor to be used by the communication utility
  */
 
-class AnalogBrakePressure : public AnalogSensor<float> {
+class BrakePressureSensor : public AnalogSensor<float> {
     public:
         void set_raw(float raw);
 };
