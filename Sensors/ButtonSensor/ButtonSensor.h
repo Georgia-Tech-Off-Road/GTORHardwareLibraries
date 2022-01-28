7964d41fcff0487754fd2f556a75d3105e761d22
Andrew Hellrigel <ahellrigel33@gmail.com>
2022-01-27 23:08:33 -0500

    Updated ButtonSensor to Block format

--- Sensors/ButtonSensor/ButtonSensor.h
+++ Sensors/ButtonSensor/ButtonSensor.h
@@ -1,46 +1,34 @@
+/**
+ * ButtonSensor.h
+ * A library to work with buttons to measure if they've been pressed
+ * 
+ * ------ Constructor ------
+ * ButtonSensor button;
+ * 
+ * ------ Usage ------
+ * button.begin();
+ *      - This will initialize the Teensy hardware so that the button sensor can work
+ * 
+ * bool button_state = button.get_data();
+ *      - This will read the state of the button
+ * 
+ * button.update();
+ *      - This will update the sensor to be used by the communication utility
+ */
+
 #ifndef BUTTONSENSOR_H
 #define BUTTONSENSOR_H
 
-#include <Sensor.h>
+#include <Block.h>
 
-class ButtonSensor : public Sensor<bool>{
+class ButtonSensor : public Block<bool>{
 private:
     uint8_t _pin;
 public:
-    ButtonSensor(){
-        _pack_bytes = sizeof(bool);
-    }
+    ButtonSensor(uint8_t pin) : _pin(pin){}
 
-    ButtonSensor(uint8_t pin) : _pin(pin){
-        _pack_bytes = sizeof(bool);
-    }
-
-    void set_pin(uint8_t pin){
-        _pin = pin;
-    }
-
-    uint8_t get_pin() {
-        return _pin;
-    }
-
-    void begin(){
-        pinMode(_pin, INPUT);
-    }
-
-    const bool& get_data (){
-        if (_type == ACTIVE) {
-            _data = digitalRead(_pin);
-        }
-        return _data;
-    }
-
-    void pack(byte* pack){
-        *((bool*)pack) = _data;
-    }
-
-    void unpack(const byte* pack){
-        _data = *((bool*)pack);
-    }
+    void begin() pinMode(_pin, INPUT);    
+    void update() this->set_data(digitalRead(_pin));
 };
 
 #endif
