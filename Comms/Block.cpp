5f9f35238b19f05bc620b2d3ec833406d1178ab9
Akash-Harapanahalli <akash@akash-pc.localdomain>
2022-01-27 23:16:02 -0500

    Added has_changed functionality to CommandBlock

--- Comms/Block.cpp
+++ Comms/Block.cpp
@@ -84,6 +84,9 @@ void DynamicBlock::unpack (const uint8_t* pack){
  */
 
 template <class DataType>
+CommandBlock<DataType>::CommandBlock() : _has_changed(0) { };
+
+template <class DataType>
 void CommandBlock<DataType>::attach_callback(std::function<void(DataType)> onchange_callback) {
     _onchange_callback = onchange_callback;
 }
@@ -96,7 +99,18 @@ void CommandBlock<DataType>::unpack (const uint8_t* pack){
 template <class DataType>
 void CommandBlock<DataType>::update() {
     if(_prev_data != this->_data) {
-        _onchange_callback(this->_data);
+        _has_changed = 1;
+        if(_onchange_callback) _onchange_callback(this->_data);
     }
     _prev_data = this->_data;
+}
+
+template <class DataType>
+bool CommandBlock<DataType>::has_changed () {
+    if(_has_changed) {
+        _has_changed = 0;
+        return 1;
+    } else {
+        return 0;
+    }
 }
--- Comms/Block.h
+++ Comms/Block.h
@@ -111,10 +111,13 @@ class CommandBlock : public Block<DataType> {
 private:
     DataType _prev_data;
     std::function<void(DataType)> _onchange_callback;
+    bool _has_changed;
 public:
+    CommandBlock();
     void attach_callback(std::function<void(DataType)> onchange_callback);
     void unpack (const uint8_t* pack);
     void update();
+    bool has_changed();
 };
 
 #include "BlockTypes.h"
