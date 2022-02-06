ba8e32364c349ee1402be232ce432dff7e7dfedb
Vishnav Deenadayalan <vishnavdeena@gmail.com>
2022-02-06 18:30:42 -0500

    Changes to ADS8688 library

--- Sensors/Analog/ADS8688.cpp
+++ Sensors/Analog/ADS8688.cpp
@@ -1,31 +1,35 @@
-#include "TeensyADC.h"
 #include <Arduino.h>
+#include <SPI.h>
+#include "ADS8688.h"
 
-void TeensyADC::attach_sensor(BaseAnalogSensor& sensor, uint8_t port){
+ADS8688::ADS8688(uint8_t CSPin){
+    _CSPin = CSPin;
+    for(uint8_t i = 0; i<8 ; i++){
+        set_port_vrange(i, 1);
+    }
+
+}
+void ADS8688::attach_sensor(BaseAnalogSensor& sensor, uint8_t port){
     // Check to see if something is already attached on the port.
     for (auto it = _sensors.begin(); it != _sensors.end(); it++){
         if((*it)->get_port() == port)
             return;
     }
     sensor.set_port(port);
-    sensor.set_vrange(0, 3.3);
+    sensor.set_vrange(0, 5.12);
     sensor.set_adc(this);
     _sensors.push_back(&sensor);
 }
 
-void TeensyADC::update_sensors() {
+void ADS8688::update_sensors() {
 	for(auto it = _sensors.begin(); it != _sensors.end(); ++it){
-		float a = analogRead((*it)->get_port());
+		float a = update_sensor((*it)->get_port());
 		(*it)->set_raw(a / get_max());
 	}
 }
 
-void TeensyADC::update_sensor(BaseAnalogSensor& sensor) {
-    float a = analogRead(sensor.get_port());
-    sensor.set_raw(a / get_max());
-}
 
-void TeensyADC::update_sensor(uint8_t port) {
+void ADS8688::update_sensor(uint8_t port) {
     for (auto it = _sensors.begin(); it != _sensors.end(); it++){
         if((*it)->get_port() == port) {
             update_sensor(*(*it));
@@ -34,15 +38,98 @@ void TeensyADC::update_sensor(uint8_t port) {
     }
 }
 
-uint32_t TeensyADC::get_max() {
-    return 1023;
+uint32_t ADS8688::get_max() {
+    return 65535;
 }
 
-uint16_t ADS8688::get_channel_voltage(command_reg_t channel){
-    uint16_t channel_voltage = 0;
+void ADS8688::set_port_vrange(uint8_t port, uint8_t vrange) {
+    // 5 = +-10.24V
+    // 4 = +-5.12V
+    // 3 = +-2.56V
+    // 2 = 0-10.24V
+    // 1 = 0-5.12V
+    voltage_reg_t voltage_range = UNSIGNED_5V12;
+    program_reg_t reg_port;
+    switch (vrange) {
+        case 1:
+            voltage_range = UNSIGNED_5V12;
+            _vmin[port] = 0;
+            _vmax[port] = 5.12;
+            break;
+        case 2:
+            voltage_range = UNSIGNED_10V24;
+            _vmin[port] = 0;
+            _vmax[port] = 10.24;
+            break;
+        case 3:
+            voltage_range = SIGNED_2V56;
+            _vmin[port] = -2.56;
+            _vmax[port] = 2.56;
+            break;
+        case 4:
+            voltage_range = SIGNED_5V12;
+            _vmin[port] = -5.12;
+            _vmax[port] = 5.12;
+            break;
+        case 5:
+            voltage_range = SIGNED_10V24;
+            _vmin[port] = -10.24;
+            _vmax[port] = 10.24;
+            break;
+        default:
+            break;
+    }
+
+    switch (port) {
+        case 0:
+            reg_port = CH0;
+            break;
+        case 1:
+            reg_port = CH1;
+            break;
+        case 2:
+            reg_port = CH2;
+            break;
+        case 3:
+            reg_port = CH3;
+            break;
+        case 4:
+            reg_port = CH4;
+            break;
+        case 5:
+            reg_port = CH5;
+            break;
+        case 6:
+            reg_port = CH6;
+            break;
+        case 7:
+            reg_port = CH7;
+            break;
+        default:
+            break;
+    }
+
     SPI.beginTransaction(SPISettings(17000000, MSBFIRST, SPI_MODE0)); // Might be mode 1
     digitalWrite(_CSPin, LOW);
-    SPI.transfer16(channel);
+    SPI.transfer16(reg_port<<8||voltage_range);
+    SPI.transfer(0);// if writing, contains data written, if reading contains data in addressed register
+    digitalWrite(_CSPin, HIGH);
+    SPI.endTransaction();
+
+    update_sensor_vrange(); // updates vrange for each sensor
+}
+
+void ADS8688::update_sensor_vrange(){
+    // goes through each sensor and updates their vrange
+    for (auto it = _sensors.begin(); it != _sensors.end(); it++){
+        it.set_vrange(_vmin[(*it)->get_port()], _vmax[(*it)->get_port()]);
+    }
+}
+
+uint16_t ADS8688::get_sample(command_reg_t port){
+    uint16_t port_voltage = 0;
+    digitalWrite(_CSPin, LOW);
+    SPI.transfer16(port);
     SPI.transfer16(0);
     digitalWrite(_CSPin, HIGH);
     SPI.endTransaction();
@@ -50,8 +137,43 @@ uint16_t ADS8688::get_channel_voltage(command_reg_t channel){
     SPI.beginTransaction(SPISettings(17000000, MSBFIRST, SPI_MODE0)); // Might be mode 1
     digitalWrite(_CSPin, LOW);
     SPI.transfer16(NO_OP);
-    channel_voltage = SPI.transfer16(0);
+    port_voltage = SPI.transfer16(0);
     digitalWrite(_CSPin, HIGH);
     SPI.endTransaction();
-    return channel_voltage;
+    return port_voltage;
+}
+
+void ADS8688::update_sensor(BaseAnalogSensor& sensor) {
+    uint8_t port = sensor.get_port();
+    uint16_t sensor_voltage = 0;
+    switch (port)
+    {
+    case 0:
+        sensor_voltage = get_sample(MAN_Ch_0);
+        break;
+    case 1:
+        sensor_voltage = get_sample(MAN_Ch_1);
+        break;
+    case 2:
+        sensor_voltage = get_sample(MAN_Ch_2);
+        break;
+    case 3:
+        sensor_voltage = get_sample(MAN_Ch_3);
+        break;
+    case 4:
+        sensor_voltage = get_sample(MAN_Ch_4);
+        break;
+    case 5:
+        sensor_voltage = get_sample(MAN_Ch_5);
+        break;
+    case 6:
+        sensor_voltage = get_sample(MAN_Ch_6);
+        break;
+    case 7:
+        sensor_voltage = get_sample(MAN_Ch_7);
+        break;
+    default:
+        break;
+    }
+    sensor.set_raw(sensor_voltage / get_max());
 }
--- Sensors/Analog/ADS8688.h
+++ Sensors/Analog/ADS8688.h
@@ -1,7 +1,10 @@
 #ifndef ADS8688_H
 #define ADS8688_H
 
+#include "Arduino.h"
 #include "ADC.h"
+
+#include <SPI.h>
 
 class ADS8688 : public ADC {
 private:
@@ -27,15 +30,31 @@ private:
 // Program Register
 // RESISTER_SELECT B[15:9] | R/W | REGISTER_DATA B[7:0]
 	typedef enum {
-		CH0 = 0x0B,
+		AUTO_SEQ_EN_R = 0x02, //Used to enable channels to be read from automatically
+		AUTO_SEQ_EN_W = 0x03,
+		CHANNEL_POWER_DOWN_R = 0x04, //Used to power down chosen channels
+		CHANNEL_POWER_DOWN_W = 0x05,
+		FEATURE_SELECT_R = 0x06, //Used for Daisy Chain
+		FEATURE_SELECT_W = 0x07,
+		//The below things can change the input voltage range
+		//REGISTER_DATA B[7:4] = 0
+		//REGISTER_DATA B[3:0] = 
+		// 0000 for input range = +-2.5*VREF
+		// 0001 for input range = +-2.5*VREF
+		// 0010 for input range = +-2.5*VREF
+		// 0101 for input range = +-2.5*VREF
+		// 0110 for input range = +-2.5*VREF
+		CH0 = 0x0B, 
 		CH1 = 0x0D,
 		CH2 = 0x0F,
 		CH3 = 0x11,
 		CH4 = 0x13,
 		CH5 = 0x15,
 		CH6 = 0x17,
-		CH8 = 0x19
+		CH7 = 0x19,
+		COMMAND_READ_BACK = 0x7E //Reads back the command word (MSB) executed in previous data frame
 	} program_reg_t;
+
 
 // Range Select Channel
 // 0000 - +-10.24V
@@ -49,9 +68,12 @@ private:
 		SIGNED_2V56 =    0b00000010,
 		UNSIGNED_10V24 = 0b00000101,
 		UNSIGNED_5V12 =  0b00000110
-	}
+	} voltage_reg_t;
 
 	uint8_t _CSPin;
+	float _vmin[8] = {0,0,0,0,0,0,0,0};
+	float _vmax[8] = {5.12,5.12,5.12,5.12,5.12,5.12,5.12,5.12};
+
 
 public:
 	ADS8688(uint8_t CSPin);
@@ -59,8 +81,10 @@ public:
 	void update_sensors();
 	void update_sensor(BaseAnalogSensor& sensor);
 	void update_sensor(uint8_t port);
+	void set_port_vrange(uint8_t port, uint8_t vrange);
+	void update_sensor_vrange();
+	uint16_t get_sample(command_reg_t port);
 	uint32_t get_max();
-	uint16_t get_channel_voltage(uint8_t channel);
 };
 
 #endif
