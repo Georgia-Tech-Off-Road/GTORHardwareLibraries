99b56dc79e8cdd7f3fdad429635d33785aab5595
Param Pithadia <ppithadia6@gatech.edu>
2022-03-08 21:55:32 -0500

    Added GPS library master

--- Sensors/SpeedSensor/SpeedSensorTesting/SpeedSensorTesting.ino
+++ Sensors/SpeedSensor/SpeedSensorTesting/SpeedSensorTesting.ino
@@ -1,14 +1,14 @@
 #include <SpeedSensor.h>
 
-SpeedSensor he_sensor_1(600, 5, 255, POSITION_AND_SPEED);
+SpeedSensor he_sensor_1(600, 23, 255, SPEED_ONLY);
 
 uint32_t temp = 0;
-bool led_on = 0;
+//bool led_on = 0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(13, OUTPUT);
+ // pinMode(13, OUTPUT);
 
   delay(100);
 }
@@ -16,9 +16,9 @@ void setup() {
 void loop() {  
   uint32_t t = micros();
   if((t - temp) > 50000) {
-    led_on = !led_on;
+   // led_on = !led_on;
     temp = t;
-    digitalWrite(13, led_on);
+  //  digitalWrite(13, led_on);
     Serial.println(he_sensor_1.get_speed());
     //Serial.print("\t");
     //Serial.println(he_sensor_1.get_position());
