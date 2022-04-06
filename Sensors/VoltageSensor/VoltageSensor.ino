11d4f151eda06de6bdd1d9dccc4eb6c9dc5ca0a1
Vishnav Deenadayalan <vishnavdeena@gmail.com>
2022-04-05 20:55:18 -0400

    voltage sensor test

--- Sensors/VoltageSensor/VoltageSensor.ino
+++ Sensors/VoltageSensor/VoltageSensor.ino
@@ -1,15 +1,14 @@
 #include "VoltageSensor.h"
-#include "ADS8688.h"
+#include "TeensyADC.h"
 #include "ClockTimer.h"
-#define TEENSY_PORT A2
+#define TEENSY_PORT A0
 
 VoltageSensor<float> volt;
-ADS8688 adc(10);
+TeensyADC tadc;
 ClockTimerf serialTimer(100);
 void setup() {
   // put your setup code here, to run once:
-  adc.attach_sensor(volt, TEENSY_PORT);
-  adc.set_port_vrange(0,ADS8688::VOLTAGE_RANGE::SIGNED_5V12);
+  tadc.attach_sensor(volt, TEENSY_PORT);
   Serial.begin(115200);
   delay(100);
   
@@ -20,10 +19,9 @@ void setup() {
 void loop() {
   volt.update();
   if(serialTimer.ready(micros())){
-    Serial.print("Voltage is: ");
+    Serial.print("Temperature is: ");
     Serial.print(volt.get_data());
-    Serial.println(" Volts");
-    
+    Serial.println(" degrees celsius");
   }
 
 }
