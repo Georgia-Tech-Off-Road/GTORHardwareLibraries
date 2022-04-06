0c059d1d1f306343b1a6abeb4b5b73e02977655d
Benjamin <bboeckman3@gatech.edu>
2022-04-05 22:16:26 -0400

    Updated BlockId.h for Strain Gauge

--- Comms/BlockId.h
+++ Comms/BlockId.h
@@ -5,7 +5,7 @@ go to Application>DataAcquisition>SensorId.py and edit the dictionary there. The
 this file by using Application>GenerateSensorIdHFile.py (It will automatically place the
 file in the correct location i.e. Libraries>Comms>BlockId.h)
 
-Generated at: 2022-04-05 22:10:35.601253
+Generated at: 2022-04-05 22:16:05.818746
 
 *******************************************************************************************/
 
@@ -91,6 +91,7 @@ enum block_id_t : uint16_t
 	WHEEL_FORCE_TRANSDUCER_ANALOG_1 = 308,	//NumBytes: [4, 4, 4, 4] - FX, FY, FZ, MX
 	WHEEL_FORCE_TRANSDUCER_ANALOG_2 = 309,	//NumBytes: [4, 4, 4, 4] - MY, MZ, Vel, Pos
 	WHEEL_FORCE_TRANSDUCER_ANALOG_3 = 310,	//NumBytes: [4, 4, 4, 4] - AccelX, AccelY
+	FORCE_STRAINGAUGE_LBS           = 311,	//NumBytes: 4
 
     /**
      * 400 - LDS SENSORS
