2d7220638c718edf46d2ef587ab098e0d8be67fb
Xander Sweet <sweetxander@gmail.com>
2022-09-22 21:04:49 -0400

    Updated strain gauge library

---
+++ Sensors/StrainGauge/StrainGauge.cpp
@@ -0,0 +1,21 @@
+#include "StrainGauge.h"
+
+void StrainGauge::update_data(){
+
+
+    set_data(-4*(get_voltage())/(2.1*(1+2*get_voltage()));
+}
+
+void StrainGauge::tare(){
+    ADC& adc = *(this->get_adc());
+    adc.update_sensor(*this);
+    _offset -= this->get_data();
+}
+
+void StrainGauge::set_scale(float scale) {
+    _scale = scale;
+}
+
+float StrainGauge::get_scale() {
+    return _scale;
+}
