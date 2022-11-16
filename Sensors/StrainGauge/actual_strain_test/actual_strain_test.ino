4b9f1299a3127d04a6aa645673bc2d26ac9c012d
nikola.r <nikola.rogers17@gmail.com>
2022-11-15 20:36:23 -0500

    Changed to print force for testing purposes

--- Sensors/StrainGauge/actual_strain_test/actual_strain_test.ino
+++ Sensors/StrainGauge/actual_strain_test/actual_strain_test.ino
@@ -1,10 +1,13 @@
 #include "StrainGauge.h"
 #include "TeensyADC.h"
 #include "ClockTimer.h"
-#define TEENSY_PORT A0
+#define TEENSY_PORT A2 //was A0
 StrainGauge strain;
 TeensyADC tadc;
 ClockTimerf serialTimer(100);
+float area = 12.5 * 1.45 * 0.000001; //cross sectional area of whatever the strain gauge is on
+float youngModulus = 70000000000; //determined by the material (aluminum for testing), multiplied by 10^10
+
 void setup() {
   // put your setup code here, to run once:
   tadc.attach_sensor(strain, TEENSY_PORT);
@@ -16,8 +19,8 @@ void loop() {
   // put your main code here, to run repeatedly:
   strain.update();
   if(serialTimer.ready(micros())){
-    Serial.print("Strain is: ");
-    Serial.println(strain.get_data());
+    Serial.print("Force is: ");
+    Serial.println(strain.get_data() * area * youngModulus);
   }
 
 }
