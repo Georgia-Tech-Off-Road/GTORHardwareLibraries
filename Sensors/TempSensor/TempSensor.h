4f390ca088e8d193a37853c75fe048a16c92f823
Samsskrit <samuelsukendro22@gmail.com>
2023-05-06 10:34:51 -0400

    Diff temp sensor equation temp fix

--- Sensors/TempSensor/TempSensor.h
+++ Sensors/TempSensor/TempSensor.h
@@ -6,7 +6,7 @@
 class TempSensor : public AnalogSensor<float> {
 public:
   void update_data() {
-    this->set_data(exp(.003 * ((this->_raw - 1) * -1024)) * 7 - 26);
+    this->set_data(144-19.7*(this->_raw)-111*((this->_raw)*(this->_raw)));
   }
 };
 
