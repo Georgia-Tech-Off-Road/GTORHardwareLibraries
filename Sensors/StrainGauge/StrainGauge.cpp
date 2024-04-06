1a57814451a82544c000244949080eba5cd698e1
Vishnav Deenadayalan <vishnavdeena@gmail.com>
2024-04-06 04:49:25 -0400

    Strain gauge changes

--- Sensors/StrainGauge/StrainGauge.cpp
+++ Sensors/StrainGauge/StrainGauge.cpp
@@ -2,21 +2,24 @@
 
 void StrainGauge::update_data(){
     //set_data(-4*(get_voltage())/(2.1*(1+2*get_voltage())));
-    float offset = 2.5;
-    float gain = 32.333;
-    float GF = 2.0;
-    float voltage = (get_voltage()-offset)/gain; // actual voltage difference across wheatstone bridge
-    float v_unstrained = 0;  //initial unstrained voltage
-    float v_ex = 3.3;
-    float v_r = (voltage - v_unstrained)/(v_ex);
-    float strain = (-4*v_r)/(GF+(1+2*v_r));
-
+    double offset = 2.5; // voltage offset 
+    double gain = 32.333333; // amplifies the voltage 
+    double GF = 2.0; // gauge factor 
+    double voltage = (get_voltage()-offset)/gain; // actual voltage difference across wheatstone bridge
+    float R1 = ((5 * 350) / (voltage + 2.5)) - 350;
+    float deltaR = (R1 - 350); // delta R
+    float strain = ((deltaR / 350)) / GF;
+    // float v_unstrained = 0;  //initial unstrained voltage
+    // float v_ex = 3.3;
+    // float v_r = (voltage - v_unstrained)/(v_ex);
+    // float strain = (-4*v_r)/(GF+(1+2*v_r));
     uint64_t youngs_modulus = 70000000000; //youngs modulus of aluminum;
-    float length = 0.0125;
-    float width = 0.00145;
-    float area = width * length //cross sectional area of material that gauge is on;
+    float length = 0.0127;
+    float width = 0.002;
+    float area = width * length; //cross sectional area of material that gauge is on;
     float force = strain * area * youngs_modulus;
-    set_data(this->get_voltage());
+
+    set_data(strain);
 }
 
 void StrainGauge::tare(){
--- Sensors/StrainGauge/actual_strain_test/actual_strain_test.ino
+++ Sensors/StrainGauge/actual_strain_test/actual_strain_test.ino
@@ -5,21 +5,39 @@
 StrainGauge strain;
 TeensyADC tadc;
 ClockTimerf serialTimer(100);
+float offset;
+uint64_t youngs_modulus = 70000000000; //youngs modulus of aluminum;
+float length = 0.0127;
+float width = 0.002;
+float area = width * length; //cross sectional area of material that gauge is on;
+float force;
+float strain_value;
+float no_ym;
 
 void setup() {
   // put your setup code here, to run once:
   tadc.attach_sensor(strain, TEENSY_PORT);
   Serial.begin(115200);
-  //strain.tare();
+  strain.update();
+  offset = strain.get_data();
+  // offset = 0;
+
   delay(100);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   strain.update();
+
   if(serialTimer.ready(micros())){
-    Serial.print("Force is: ");
-    Serial.println(strain.get_data());
+    //Serial.printf("Force is: %.10f\n", strain.get_data());
+    strain_value = strain.get_data();
+    strain_value = strain_value - offset;
+    force = (strain_value * area * youngs_modulus)/4.448;
+    no_ym = strain_value * area;
+    Serial.printf("strain is: %.10f\n", strain_value);
+    Serial.printf("Force is: %.10f\n", force);
+    Serial.printf("no_ym is: %.10f\n", no_ym);
   }
 
 }
